bf31776b959664816606b7e97ec802e77e8a85fe
Arun Verma <arun9024@gmail.com>
2018-05-24 22:45:37 +0200

    txt file of refugee tweets

--- bin/csv.py
+++ bin/csv.py
@@ -2,7 +2,7 @@ import json
 import re
 import csv
 
-with open('tweet_employee.json', 'r') as handle:
+with open('tweet_refugee.json', 'r') as handle:
     text_data = handle.read()
     text_data = '[' + re.sub(r'\}\s\{', '},{', text_data) + ']'
     json_data = json.loads(text_data)
