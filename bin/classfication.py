021461d8adf74866a8ef76b500798b33cf03f134
sumeet singh <singhsumeet2007@gmail.com>
2018-06-26 17:45:50 +0200

    SVM and ploting decision tree

--- bin/classfication.py
+++ bin/classfication.py
@@ -1,19 +1,28 @@
-from sklearn import svm
+from sklearn.svm import SVC
 from feature_extraction import tf_idf_vect_feature_vector
-from sklearn.tree import DecisionTreeClassifier
-import numpy as np
+from sklearn.tree import DecisionTreeClassifier,export_graphviz
+from IPython.display import Image
+import pydotplus
 
 def decision_tree_classification():
     df_test,df_train,df = tf_idf_vect_feature_vector()
     tree = DecisionTreeClassifier()
-    tree.fit(df_train['tweets_vec'].tolist(), df_train['tag'].tolist())
-    tree.predict(df_test['tweets_vec'].tolist(),df_test.tag.tolist())
+    tree = tree.fit(df_train['tweets_vec'].tolist(), df_train['tag'].tolist())
+    dotfile = open("dtree2.dot", 'w')
+    export_graphviz(tree, out_file=dotfile,filled=True, rounded=True,special_characters=True)
+    graph = pydotplus.graph_from_dot_data(dotfile.getvalue())
+    Image(graph.create_png())
+    predict =tree.predict(df_test['tweets_vec'].tolist(),df_test.tag.tolist())
+    print(predict)
+    return predict
 
 def svm_classification():
-    df_test, df_train, train_vect, df = tf_idf_vect_feature_vector()
-    tree = DecisionTreeClassifier()
-    tree.fit(train_vect, df_train['tag'].tolist())
-    tree.predict(df_test.Tweets.tolist(), df_test.tag.tolist())
+    df_test, df_train, df = tf_idf_vect_feature_vector()
+    clf = SVC()
+    clf.fit(df_train['tweets_vec'].tolist(), df_train['tag'].tolist())
+    predict = clf.predict(df_test['tweets_vec'].tolist(), df_test.tag.tolist())
+    print(predict)
+    return predict
 
 
 
--- bin/init.py
+++ bin/init.py
@@ -1,12 +1,13 @@
 
 from text_processing import text_processed
 from feature_extraction import tf_idf_vect_feature_vector
-from classfication import decision_tree_classification
+from classfication import decision_tree_classification,svm_classification
 
 
 #array = text_processed()
 #print(array)
-vec = decision_tree_classification()
+#vec = decision_tree_classification()
+vec = svm_classification()
 
 print(vec)
 #print(vec.toarray())
