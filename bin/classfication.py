740f36f3837f29a349e77f509829eaa020944a60
sumeet singh <singhsumeet2007@gmail.com>
2018-06-27 13:57:46 +0200

    plotting SVM and ploting decision tree

--- bin/classfication.py
+++ bin/classfication.py
@@ -7,17 +7,21 @@ import pydotplus
 from sklearn.externals.six import StringIO
 from mlxtend.plotting import plot_decision_regions
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 def decision_tree_classification():
     df_test,df_train,df = tf_idf_vect_feature_vector()
     tree = DecisionTreeClassifier()
-    tree = tree.fit(df_train['tweets_vec'].tolist(), df_train['tag'].tolist())
+    tree.fit(df_train['tweets_vec'].tolist(), df_train['tag'].tolist())
     print(tree)
+    y_pred = tree.predict(df_test['tweets_vec'].tolist())
+    accuracy = accuracy_score(df_test['tag'].tolist(), y_pred) * 100
+    print("Accuracy score is :",accuracy)
     #dotfile = open("dtree2.dot", 'w')
-    dotfile = StringIO()
-    export_graphviz(tree, out_file=dotfile,filled=True, rounded=True,special_characters=True)
-    graph = pydotplus.graph_from_dot_data(dotfile.getvalue())
-    Image(graph.create_png())
+    # dotfile = StringIO()
+    # export_graphviz(tree, out_file=dotfile,filled=True, rounded=True,special_characters=True)
+    # graph = pydotplus.graph_from_dot_data(dotfile.getvalue())
+    # graph.write_png('plot.png')
     #predict =tree.predict(df_test['tweets_vec'].tolist(),df_test.tag.tolist())
     #print(predict)
     return tree
@@ -27,14 +31,13 @@ def svm_classification():
     clf = SVC()
     clf.fit(df_train['tweets_vec'].tolist(), df_train['tag'].tolist())
     # Plot Decision Region using mlxtend's awesome plotting function
-    print(clf.score())
+   # print(clf.score())
     plot_decision_regions(X=df_train['tweets_vec'].tolist(),y=df_train['tag'].tolist(),clf=clf,legend=2)
 
     # Update plot object with X/Y axis labels and Figure Title
     #plt.xlabel(X.columns[0], size=14)
     #plt.ylabel(X.columns[1], size=14)
     plt.title('SVM Decision Region Boundary', size=16)
-    plt.plot
     plt.show()
 
     predict = clf.predict(df_test['tweets_vec'].tolist(), df_test.tag.tolist())
