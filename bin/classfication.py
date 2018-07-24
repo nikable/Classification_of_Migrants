1c951b626f9e46e6bc487a120f5eb9aef54fb3fd
hirpara <hirparadivya21@gmail.com>
2018-07-24 12:06:04 +0200

    evaluation

--- bin/classfication.py
+++ bin/classfication.py
@@ -8,6 +8,7 @@ from sklearn.externals.six import StringIO
 from mlxtend.plotting import plot_decision_regions
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
 def decision_tree_classification():
     df_test,df_train,df = tf_idf_vect_feature_vector()
@@ -17,6 +18,12 @@ def decision_tree_classification():
     y_pred = tree.predict(df_test['tweets_vec'].tolist())
     accuracy = accuracy_score(df_test['tag'].tolist(), y_pred) * 100
     print("Accuracy score is :",accuracy)
+    
+    # Calculate metrics globally by counting the total true positives, 
+    # false negatives and false positives.
+    f1 = f1_score(df_test['tag'].tolist(), y_pred, average='micro')
+    print("f1_score is :", f1)
+
     #dotfile = open("dtree2.dot", 'w')
     # dotfile = StringIO()
     # export_graphviz(tree, out_file=dotfile,filled=True, rounded=True,special_characters=True)
@@ -33,15 +40,19 @@ def svm_classification():
     # Plot Decision Region using mlxtend's awesome plotting function
    # print(clf.score())
     plot_decision_regions(X=df_train['tweets_vec'].tolist(),y=df_train['tag'].tolist(),clf=clf,legend=2)
-
     # Update plot object with X/Y axis labels and Figure Title
     #plt.xlabel(X.columns[0], size=14)
     #plt.ylabel(X.columns[1], size=14)
     plt.title('SVM Decision Region Boundary', size=16)
     plt.show()
 
-    predict = clf.predict(df_test['tweets_vec'].tolist(), df_test.tag.tolist())
+    #predict = clf.predict(df_test['tweets_vec'].tolist(), df_test.tag.tolist())
+    predict = clf.predict(df_test['tweets_vec'].tolist(), df_test['tag'].tolist())
     print(predict)
+
+    f1 = f1_score(df_test['tag'].tolist(), predict, average='micro')
+    print("f1_score is :", f1)
+
     return predict
 
 
